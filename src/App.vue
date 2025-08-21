38c6e2629b255d557035785b26e731cc2c2ea0dc
XverLxrd <tur-2002@mail.ru>
2025-08-21 17:09:55 +0700

    #init: иницализировал проект

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
